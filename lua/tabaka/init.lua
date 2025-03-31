5647af96038dd7eec43baed54c16a4f490111a76
nyngwang <nyngwang@gmail.com>
2025-04-01 02:59:14 +0800

    add: checkboxes to plan

--- lua/tabaka/init.lua
+++ lua/tabaka/init.lua
@@ -7,7 +7,7 @@
 --   - after reordering of tabpage
 --   - after restoring from a session
 --     - NOTE: `tabnr == tabid` for every tabpage after restoring a session
--- - it should execute an editing commands only if the tabaka window is presented
+-- - [v] it should execute an editing commands only if the tabaka window is presented
 --
 --
 -- tabaka can create & maintain markdown files for users
@@ -18,24 +18,24 @@
 --
 --
 -- tabaka can be customized by the following properties:
--- - a user can define the width of the tabaka window
--- - a user can define the position of the tabaka window (left/right/top/down)
--- - a user can supply a custom template for the markdown, instead of the default one
--- - a user can define custom placeholder for each template
+-- - [ ] a user can define the width of the tabaka window
+-- - [ ] a user can define the position of the tabaka window (left/right/top/down)
+-- - [ ] a user can supply a custom template for the markdown, instead of the default one
+-- - [ ] a user can define custom placeholder for each template
 --
 --
 -- tabaka will create user commands, `:tabaka commands`, for the following use cases:
--- - a user can create the markdown for the current tabpage
--- - a user can toggle the markdown as the very-{top,down,left,right} fixed-{width,height} window
--- - a user can modify the markdown easily (tabaka<=nvim)
---   - update the task title of the current tabpage
---   - update the task description of the current tabpage
---   - add/del an item in the note list of the current tabpage
+-- - [ ] a user can create the markdown for the current tabpage
+-- - [ ] a user can toggle the markdown as the very-{top,down,left,right} fixed-{width,height} window
+-- - [ ] a user can modify the markdown easily (tabaka<=nvim)
+--   - [ ] update the task title of the current tabpage
+--   - [ ] update the task description of the current tabpage
+--   - [ ] add/del an item in the note list of the current tabpage
 --     - first prompt the user for the index to insert after
 --     - second prompt the user for the note text
---   - add/del an item in the note list to the "doing"
---   - add/del an item in the note list to the "done"
---   - add/del an item in the buffer list of the current tabpage
+--   - [ ] add/del an item in the note list to the "doing"
+--   - [ ] add/del an item in the note list to the "done"
+--   - [ ] add/del an item in the buffer list of the current tabpage
 --     - use the buffer in the current window
 --     - use the buffer id provided by the user
 --
