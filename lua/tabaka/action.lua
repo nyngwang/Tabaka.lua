69103becf1a312253d3d973e279013a691d76499
nyngwang <nyngwang@gmail.com>
2025-04-01 01:35:04 +0800

    refactor(action): better action structure
    this is a big one:
    1. allow more than markdown.
    2. categorize actions.
    3. use [action, object]-pair for clarity.

--- lua/tabaka/action.lua
+++ lua/tabaka/action.lua
@@ -2,61 +2,136 @@ local W = require('tabaka.window')
 local M = {}
 
 
-M.object = {
-  add_note = function ()
-    -- example1: already 3 notes, add_note 2, then note 2 the new note.
-    -- example2: already 3 notes, add_note $, then note 4 the new note.
-    return { '1', '2', '$' }
-  end,
-  delete_note = function ()
-    return { '1', '2' }
-  end,
-  pin_note = function ()
-    return { '1', '2' }
-  end,
-  add_buffer = function ()
-    return { '42', '69' }
-  end,
-  delete_buffer = function ()
-  end
-}
-
-
-M.action = {
-  update_task_title = function ()
-  end,
-  udpate_task_details = function ()
-  end,
-  add_note = function ()
-  end,
-  delete_note = function ()
-  end,
-  pin_note = function ()
-  end,
-  add_buffer = function ()
-  end,
-  delete_buffer = function ()
-  end
-}
-
-
-function M.toggle_window_tabaka()
-  if -- already presented in the current tabpage.
-    W.get_window_tabaka()[1]
-    then -- try close it.
-    W.close_window_tabaka(W.get_window_tabaka()[2])
-    return
-  end
-  -- otherwise, we try to open the tabaka window.
-  local ok, winid_tabaka = W.create_window_tabaka()
-  if not ok then return end
-  W.load_markdown_in_window(winid_tabaka)
+local get_current_filetype = function ()
+  return 'markdown'
 end
 
 
-function M.dispatcher_command(fargs)
-  -- TODO: update after testing.
-  M.toggle_window_tabaka()
+M.action = {
+  -- the first layer is the document type; we only support markdown now.
+  markdown = {
+    -- the second layer is the command category.
+    create = {
+      -- the third layer is the command itself.
+      create_with_template = {
+        -- the fourth layer is a list: [action, object].
+        function (args)
+        end,
+        function ()
+        end,
+      },
+    },
+    window = {
+      toggle_window = {
+        function (args)
+          -- TODO: check creation done.
+
+          if -- already presented in the current tabpage.
+            W.get_window_tabaka()[1]
+            then -- try close it.
+            W.close_window_tabaka(W.get_window_tabaka()[2])
+            return
+          end
+          -- otherwise, we try to open the tabaka window.
+          local ok, winid_tabaka = W.create_window_tabaka()
+          if not ok then return end
+          W.load_markdown_in_window(winid_tabaka)
+        end,
+        function ()
+          return { 'H', 'J', 'K', 'L' }
+        end,
+      }
+    },
+    edit = {
+      update_task_title = {
+        function () end,
+        function () return {} end,
+      },
+      udpate_task_details = {
+        function () end,
+        function () return {} end,
+      },
+      add_note = {
+        function () end,
+        function ()
+          -- example1: already 3 notes, add_note 2, then note 2 the new note.
+          -- example2: already 3 notes, add_note $, then note 4 the new note.
+          return { '1', '2', '$' }
+        end,
+      },
+      delete_note = {
+        function () end,
+        function () return { '1', '2' } end,
+      },
+      pin_note = {
+        function () end,
+        function () return { '1', '2' } end,
+      },
+      add_buffer = {
+        function () end,
+        function () return { '42', '69' } end,
+      },
+      delete_buffer = {
+        function () end,
+        function () return { '42', '69' } end,
+      },
+    },
+  },
+}
+
+
+function M.get_actions_all_cats(filetype)
+  if -- the caller does not know the context.
+    not filetype
+    then -- use the internal default.
+    filetype = get_current_filetype()
+  end
+
+  local actions_all = {}
+  for _, actions in pairs(M.action[filetype]) do
+    for k, v in pairs(actions) do
+      actions_all[k] = v
+    end
+  end
+  return actions_all
+end
+
+
+function M.dispatch_command(fargs)
+  local argc = #fargs
+  if -- is the laziest command.
+    argc == 0 then
+    M.action[('%s.window.toggle_window'):format(get_current_filetype())]({})
+    return
+  end
+
+  local action = fargs[1]
+  local actions = M.get_actions_all_cats()
+
+  if -- action is not valid.
+    not actions[action] then
+    -- TODO: but we should allow a command like `:Baka vsplit L`.
+    print(('Tabaka: No such action: %s'):format(action))
+    return
+  end
+
+  local args = { unpack(fargs, 2) }
+  if argc == 1
+    then -- special case for `{nil}`.
+    args = {}
+  end
+
+  if -- the tabaka window does not exist.
+    not W.get_window_tabaka()[1]
+    and -- it's an editing command.
+    M.action[('%s.edit'):format(get_current_filetype())][action]
+    then -- abort editing commands.
+    print(('Tabaka: Failed to run command: %s, toggle the window first.'):format(action))
+    return
+  end
+
+  -- finally, peacefully...
+  actions[action][1](args)
 end
 
 
--- lua/tabaka/cmdline.lua
+++ lua/tabaka/cmdline.lua
@@ -7,7 +7,7 @@ function M.create_user_commands()
   vim.api.nvim_create_user_command(C.NAME_COMMAND, function (opts)
     local fargs = opts.fargs
     if #fargs > 2 then return end
-    V.dispatcher_command(fargs)
+    V.dispatch_command(fargs)
   end, {
     nargs = '*',
     complete = function (cword, cmdline)
@@ -24,14 +24,15 @@ function M.create_user_commands()
         count_sep == 0
         then return {} end
 
-      local actions = vim.tbl_keys(V.action)
+      local actions = V.get_actions_all_cats()
+      local keys_actions = vim.tbl_keys(actions)
 
       if -- complete the first argument.
         count_sep == 1
-        then -- should return a list of actions.
-        if cword == '' then return actions end
+        then -- should return a list of action names.
+        if cword == '' then return keys_actions end
         local matches = {}
-        for _, action in ipairs(actions) do
+        for _, action in ipairs(keys_actions) do
           if action:sub(1, #cword) == cword then
             matches[#matches+1] = action
           end
@@ -42,13 +43,11 @@ function M.create_user_commands()
       if -- complete the second argument.
         count_sep == 2
         then -- should return the objects defined by each action.
-        -- the first argument should be an action, but need check.
+        -- we expect the first argument to be an action, but who knows?
         local action = words[2]
-        if not V.action[action] then return {} end
-        if -- the action takes no argument.
-          not V.object[action]
-          then return {} end
-        return V.object[action]()
+        if not actions[action] then return {} end
+        -- each action knows how to get its own objects.
+        return actions[action][2]
       end
       return {} -- don't provide completion for #args > 2.
     end
