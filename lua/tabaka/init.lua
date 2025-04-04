791e59cf4eb9e34f0c0e44c6058b7811b6701145
nyngwang <nyngwang@gmail.com>
2025-04-04 10:48:36 +0800

    update: plan
    generalize the idea for all kinds of files, not just markdown ;)

--- lua/tabaka/init.lua
+++ lua/tabaka/init.lua
@@ -1,40 +1,28 @@
 --[[
 PLUGIN PLAN:
 
+tabaka can provide the following functionalities:
+- [ ] for each tabpage, a user can {toggle} the tabaka window on one of the {top,bottom,left,right}-side
+  - [ ] a user can only toggle {one} tabaka window at a time for each tabpage
+  - [ ] open the tabaka window send the cursor into it
+  - [ ] close the tabaka window put the cursor back to where it was
+- [ ] for each tabpage, a user can {link} {one} file on each of the {top,bottom,left,right}-side
+  - [ ] to {link} a file to one of the sides, open the file in the tabaka window on that side
+  - [ ] the {create} command is available inside the tabaka window to create a file from {bundled,provided} template
+  - [ ] the linked file will be shown when the tabaka window is toggled on that side
+- [ ] for each tabpage, everything still work after {tabpage-reordering,session-restoring}
+  - close a tabpage will not delete those linked files, but no way to restore the state
+- [ ] for each tabpage, only {filetype} commands of the linked file will be available
+  - [ ] if there is no tabaka window, only the {toggle} command will be available
+
 tabaka should follow these invariants:
 - it should not create/delete any markdown file implicitly
-- it should open the correct markdown file for each tabpage
-  - after reordering of tabpage
-  - after restoring from a session
-    - NOTE: `tabnr == tabid` for every tabpage after restoring a session
-- [v] it should execute an editing commands only if the tabaka window is presented
-
-tabaka can create & maintain markdown files for users
-- a user needs to call the creation command before using tabaka
-  - notify the user the markdown has NOT been created
-  - notify the user the markdown has been created
-- a user can choose a template to use for the underlying markdown
 
 tabaka can be customized by the following properties:
-- [ ] a user can define the width of the tabaka window
-- [ ] a user can define the position of the tabaka window (left/right/top/down)
-- [ ] a user can supply a custom template for the markdown, instead of the default one
-- [ ] a user can define custom placeholder for each template
+- [ ] a user can define the width/height of the tabaka window on each side of a tabpage
+- [ ] a user can supply custom templates for a filetype
 
-tabaka will create user commands, `:tabaka commands`, for the following use cases:
-- [ ] a user can create the markdown for the current tabpage
-- [ ] a user can toggle the markdown as the very-{top,down,left,right} fixed-{width,height} window
-- [ ] a user can modify the markdown easily (tabaka<=nvim)
-  - [ ] update the task title of the current tabpage
-  - [ ] update the task description of the current tabpage
-  - [ ] add/del an item in the note list of the current tabpage
-    - first prompt the user for the index to insert after
-    - second prompt the user for the note text
-  - [ ] add/del an item in the note list to the "doing"
-  - [ ] add/del an item in the note list to the "done"
-  - [ ] add/del an item in the buffer list of the current tabpage
-    - use the buffer in the current window
-    - use the buffer id provided by the user
+END OF PLUGIN PLAN
 ]]
 local L = require('tabaka.cmdline')
 local M = {}
