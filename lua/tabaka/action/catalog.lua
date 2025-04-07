ded12ba6137e8b82b0fbd91a9ee5392e628c5719
nyngwang <nyngwang@gmail.com>
2025-04-07 14:28:27 +0800

    impl(catalog): context-aware cmdline action-name completion

--- lua/tabaka/action/catalog.lua
+++ lua/tabaka/action/catalog.lua
@@ -1,3 +1,4 @@
+local W = require('tabaka.window')
 local P = require('tabaka.filepath')
 local M = {
   modname_action = {},
@@ -15,7 +16,12 @@ function M.get_current_filetype()
   -- the default value for the first index of `M.action` when the value
   -- is missing in the context. In the future, we can provide a command
   -- to change the return value, and thus swap the entire set of commands.
-  return 'markdown'
+  if W.get_winid_tabaka()[1] then
+    local bufnr_win_tabaka = vim.api.nvim_win_get_buf(W.get_winid_tabaka()[2])
+    local filetype = vim.api.nvim_get_option_value('filetype', { buf = bufnr_win_tabaka })
+    return #filetype > 0 and filetype or 'none'
+  end
+  return 'none'
 end
 
 
@@ -24,16 +30,6 @@ function M.get_all_actions(pattern_filetype)
     return vim.fn.fnamemodify(name_folder, ':h:t')
   end, vim.fn.globpath(M.filepath_head, '*/', false, true))
 
-  if not pattern_filetype then
-    pattern_filetype = M.get_current_filetype()
-  end
-  if type(pattern_filetype) == 'string' then
-    names_folders = vim.tbl_filter(function (name_folder)
-      return name_folder:match(pattern_filetype)
-    end, names_folders)
-
-    names_folders = { 'common', unpack(names_folders) }
-  end
   local actions = {}
 
   for _, name_folder in ipairs(names_folders) do
@@ -42,6 +38,16 @@ function M.get_all_actions(pattern_filetype)
       actions[#actions+1] = action
     end
   end
+
+  if not pattern_filetype then
+    pattern_filetype = M.get_current_filetype()
+  end
+
+  actions = vim.tbl_filter(function (action)
+    return action.filetype == 'common'
+      or action.type == 'create'
+      or action.filetype:match(pattern_filetype)
+  end, actions)
 
   return actions
 end
