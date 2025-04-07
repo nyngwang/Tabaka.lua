1de2510f852110ee0cdd36d34228e3d9d7c163e9
nyngwang <nyngwang@gmail.com>
2025-04-07 10:15:35 +0800

    add(defaults): callback on_open set window height/width

--- lua/tabaka/defaults.lua
+++ lua/tabaka/defaults.lua
@@ -9,4 +9,24 @@ M.constants = {
 }
 
 
+M.default_opts = {
+  callback = {
+    on_open = {
+      function (args)
+        local h_edtior = vim.api.nvim_get_option_value('lines', { scope = 'global' })
+        local w_editor = vim.api.nvim_get_option_value('columns', { scope = 'global' })
+
+        if args.HJKL == 'K' then
+          vim.api.nvim_win_set_height(args.winid, math.floor(h_edtior/4))
+        elseif args.HJKL == 'J' then
+          vim.api.nvim_win_set_height(args.winid, math.floor(h_edtior/3))
+        else
+          vim.api.nvim_win_set_width(args.winid, math.floor(w_editor/5))
+        end
+      end
+    }
+  }
+}
+
+
 return M
